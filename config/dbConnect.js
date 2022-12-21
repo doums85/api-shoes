938bd85959ef4f626586d9bb102a5f37b00768f6
MAMADOU SECK <mamadouseck@Mac-mini-de-MAMADOU.local>
2022-12-21 05:35:59 +0100

    done

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose';
 
 const dbConnect = () => {
-  mongoose
-    .connect(
-      'mongodb+srv://admin:admin@cluster0.whoer.mongodb.net/e-commerce?retryWrites=true&w=majority'
-    )
-    .then(() => console.log('MongoDB Connect'));
+  mongoose.connect(
+    'mongodb+srv://admin:admin@cluster0.whoer.mongodb.net/e-commerce?retryWrites=true&w=majority'
+  );
 };
 
 export default dbConnect;
--- controller/productController.js
+++ controller/productController.js
@@ -2,46 +2,54 @@ import Product from '../models/productModel';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
 import APIFeatures from '../utils/apiFeatures';
+const excludes =
+  '-__v -Promo -heel -lenght -material -production -productname -price_novat -code';
+export const getAllProducts = catchAsync(
+  async (req, res, next) => {
+    let filter = { sub_category: 'Sneakers' };
+    const { limit } = req.query;
 
-export const getAllProducts = catchAsync(async (req, res, next) => {
-  let filter = { sub_category: 'Sneakers' };
-  const { limit } = req.query;
+    const features = new APIFeatures(
+      Product.find(filter),
+      req.query
+    )
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-  const features = new APIFeatures(Product.find(filter), req.query)
-    .filter()
-    .sort()
-    .limitFields()
-    .paginate();
+    const data = await features.query
+      .select(excludes)
+      .limit(limit || 200);
 
-  const data = await features.query
-    .select('-__v -Promo -heel -lenght -material -production -productname -price_novat -code')
-    .limit(limit || 200);
+    if (!data || data.length === 0)
+      next(new AppError('No document found', 404));
 
-  if (!data) next(new AppError('No document found with that ID', 404));
-
-  res.status(200).json({
-    status: 'success',
-    results: data.length,
-    data: {
+    res.status(200).json({
+      status: 'success',
+      results: data.length,
       data,
-    },
-  });
-});
+    });
+  }
+);
 
 export const getOne = catchAsync(async (req, res, next) => {
   let data;
 
   req.url.includes('model')
-    ? (data = await Product.find({ 'model.model_id': req.query.id }))
-    : (data = await Product.findById(req.query.id));
+    ? (data = await Product.find({
+        'model.model_id': req.query.id,
+      }).select(excludes))
+    : (data = await Product.findById(req.query.id).select(excludes));
 
-  if (!data) next(new AppError('No document found with that ID', 404));
+  if (!data)
+    next(
+      new AppError('No document found with that ID', 404)
+    );
 
   res.status(200).json({
     status: 'success',
     results: data.length,
-    data: {
-      data,
-    },
+    data,
   });
 });
